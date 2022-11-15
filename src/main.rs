0705f0cdc2f428ac9f65e4c8e9d6e02a96925d29
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-11-15 05:15:34 +0100

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+mod cli;
+
+use clap::Parser;
+use dotenv::dotenv;
+use std::env;
+use tokio;
+use cli::Cli;
+
+
+
+#[tokio::main()]
+async fn main(){
+    let mcli = Cli::parse();
+    dotenv().ok();
+    let url = env::var("URL").expect("URL not set");
+    let token = env::var("TOKEN").expect("TOKEN not set");
+
 }
